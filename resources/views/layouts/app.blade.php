ee3f9b40ccba7cb44c110e0b301aaef9a92dd649
jeyco <jeyc.castro@duocuc.cl>
2025-05-19 01:46:57 -0400

    FEAT: se agrega estilo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <title>EasyPlan</title>
     @vite('resources/css/app.css')
     @livewireStyles
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body class="bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200">
     @auth
