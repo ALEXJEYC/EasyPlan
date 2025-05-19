e7cf3a5868231667ef8eaacecae2e5c91a7c668f
jeyco <jeyc.castro@duocuc.cl>
2025-05-19 18:11:47 -0400

    fix: se elimina script sin utilidad

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
 
     <!-- Font Awesome y Alpine -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
 <body class="bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200">
