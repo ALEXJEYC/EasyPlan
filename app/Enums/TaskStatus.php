117dba346c9395162605a1b487d38630c90b0318
jeyco <jeyc.castro@duocuc.cl>
2025-05-26 02:02:15 -0400

    feat: se modifica enums

--- app/Enums/TaskStatus.php
+++ app/Enums/TaskStatus.php
@@ -4,13 +4,17 @@ namespace App\Enums;
 
 enum TaskStatus: string
 {
-    case PENDING = 'pending';
+    case PENDING = 'pending';//util
     case IN_PROGRESS = 'in_progress';
-    case SUBMITTED = 'submitted';
-    case APPROVED = 'approved';
-    case REJECTED = 'rejected';
-    case NEEDS_REVISION = 'needs_revision';
-    case COMPLETED = 'completed';
+    case SUBMITTED = 'submitted';//util
+    case APPROVED = 'approved';//util
+    case REJECTED = 'rejected'; //util
+    case NEEDS_REVISION = 'needs_revision'; // util
+    // case COMPLETED = 'completed';
+        public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
 
     public static function getTransitions(string $currentStatus): array
     {
@@ -19,6 +23,7 @@ enum TaskStatus: string
             self::IN_PROGRESS->value => [self::SUBMITTED->value],
             self::SUBMITTED->value => [self::APPROVED->value, self::REJECTED->value, self::NEEDS_REVISION->value], // Añadido NEEDS_REVISION
             self::REJECTED->value => [self::IN_PROGRESS->value],
+            self::APPROVED->value => [],
             self::NEEDS_REVISION->value => [self::IN_PROGRESS->value], // Añadido transición desde NEEDS_REVISION
             default => []
         };
@@ -39,7 +44,7 @@ enum TaskStatus: string
             self::APPROVED => 'Aprobado',
             self::REJECTED => 'Rechazado',
             self::NEEDS_REVISION => 'Necesita Revisión',
-            self::COMPLETED => 'Completado', // utilizado
+            // self::COMPLETED => 'Completado', // utilizado
         };
     }
 
