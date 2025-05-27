6e33de3699ff96ead821f832a6578ac0c6268a66
jeyco <jeyc.castro@duocuc.cl>
2025-05-26 21:00:48 -0400

    feat: se agrega el revisor en la columna de revisor en el panel de historia

--- app/Livewire/TaskHistory.php
+++ app/Livewire/TaskHistory.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\Project;
 use App\Models\Task;
+use App\Models\TaskReview;
+use App\Enums\TaskStatus;
 
 class TaskHistory extends Component
 {
@@ -35,31 +37,36 @@ class TaskHistory extends Component
         $this->reset(['searchTitle', 'searchAssignedUser', 'searchApprover', 'searchDate']);
         $this->resetPage();
     }
-   public function render()
-   {
-       $tasks = Task::with(['taskUsers.user', 'reviews.reviewer'])
-           ->where('project_id', $this->project->id)
-           ->where('status', 'approved')
-           ->when($this->searchTitle, function ($query) {
-               $query->where('title', 'like', '%'.$this->searchTitle.'%');
-           })
-           ->when($this->searchAssignedUser , function ($query) {
-               $query->whereHas('taskUsers.user', function ($q) {
-                   $q->where('name', 'like', '%'.$this->searchAssignedUser .'%');
-               });
-           })
-           ->when($this->searchApprover, function ($query) {
-               $query->whereHas('reviews.reviewer', function ($q) {
-                   $q->where('name', 'like', '%'.$this->searchApprover.'%');
-               });
-           })
-           ->when($this->searchDate, function ($query) {
-               $query->whereDate('updated_at', $this->searchDate);
-           })
-           ->orderBy('updated_at', 'desc')
-           ->paginate(5);
+    public function render()
+    {
+        $tasks = Task::with(['taskUsers.user', 'reviews' => function ($query) {
+                $query->where('task_reviews.status', 'approved')->with('reviewer');
+            }])
+            ->where('project_id', $this->project->id)
+            ->where('tasks.status', TaskStatus::APPROVED->value) // tabla especificada
+            ->when($this->searchTitle, function ($query) {
+                $query->where('title', 'like', '%'.$this->searchTitle.'%');
+            })
+            ->when($this->searchAssignedUser , function ($query) {
+                $query->whereHas('taskUsers.user', function ($q) {
+                    $q->where('name', 'like', '%'.$this->searchAssignedUser .'%');
+                });
+            })
+            ->when($this->searchApprover, function ($query) {
+                $query->whereHas('reviews', function ($q) {
+                    $q->where('task_reviews.status', 'approved')
+                    ->whereHas('reviewer', function ($q2) {
+                        $q2->where('name', 'like', '%'.$this->searchApprover.'%');
+                    });
+                });
+            })
+            ->when($this->searchDate, function ($query) {
+                $query->whereDate('updated_at', $this->searchDate);
+            })
+            ->orderBy('updated_at', 'desc')
+            ->paginate(5);
 
-       return view('livewire.task-history', compact('tasks'));
-   }
-   
+        return view('livewire.task-history', compact('tasks'));
+    }
+
 }
